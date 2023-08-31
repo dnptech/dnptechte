a593802a78253b59ba4e52f63aefe225b19762bd
dnptech <138825551+dnptech@users.noreply.github.com>
2023-08-31 17:48:12 -0300

    Add files via upload

--- Pre-requisitos.sh
+++ Pre-requisitos.sh
@@ -1,18 +1,27 @@
-# Actualizar el sistema e instalar dependencias
+#!/bin/bash
+
+# Obtener la ubicación actual del script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Actualizar paquetes e instalar dependencias
 sudo apt -y update
-sudo apt install -y wget curl unzip
-sudo apt-get install libpq-dev
-sudo pip install psycopg2-binary pandas psycopg2 selenium
+sudo apt install -y wget curl unzip libvulkan1
 
-# Descargar e instalar Google Chrome
+# Instalar libu2f-udev
+wget http://archive.ubuntu.com/ubuntu/pool/main/libu/libu2f-host/libu2f-udev_1.1.4-1_all.deb
+sudo dpkg -i libu2f-udev_1.1.4-1_all.deb
+
+# Instalar Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# Descargar, instalar y configurar ChromeDriver
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
-unzip -o /tmp/chromedriver_linux64.zip -d /tmp/
-sudo chmod +x /tmp/chromedriver
-sudo mv /tmp/chromedriver /usr/local/bin/chromedriver
+# Obtener la versión más reciente del Chrome Driver
+CHROME_DRIVER_VERSION=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json | python3 -c "import sys, json; print(json.load(sys.stdin)['channels']['Stable']['version'])")
+echo "Latest Chrome Driver version: $CHROME_DRIVER_VERSION"
 
-echo "Configuración completa."
+# Descargar y configurar el Chrome Driver en el mismo directorio del script
+wget -N "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROME_DRIVER_VERSION/linux64/chromedriver_linux64.zip" -P "$SCRIPT_DIR"
+unzip -o "$SCRIPT_DIR/chromedriver_linux64.zip" -d "$SCRIPT_DIR"
+chmod +x "$SCRIPT_DIR/chromedriver_linux64/chromedriver"
+
+echo "listo"
