5498e69f5b3d506efdd9f48696a670e82c57045c
dnptech <138825551+dnptech@users.noreply.github.com>
2023-09-11 19:36:26 -0300

    Update Pre-requisitos.sh

--- Pre-requisitos.sh
+++ Pre-requisitos.sh
@@ -1,11 +1,11 @@
 
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Obtener la versión más reciente del Chrome Driver
-wget "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
+sudo wget "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
 
-unzip -o "chromedriver_linux64.zip" 
-chmod +x "chromedriver_linux64/chromedriver"
+sudo unzip -o "chromedriver_linux64.zip" 
+sudo chmod +x "chromedriver_linux64/chromedriver"
 
 echo "listo"
